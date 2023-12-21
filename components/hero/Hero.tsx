fdcbd719dd541965e2f4673fae848ce1e9f8b4e6
abyanmajid <abyan@abydyl.net>
2023-12-21 15:48:13 +0700

    Refined landing page

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,54 +1,19 @@
+import HeroForm from "@/components/Forms/HeroForm"
+
 export default function Hero() {
   return (
     <section className="bg-center bg-no-repeat bg-hero bg-blend-multiply">
       <div className="px-4 mx-auto max-w-screen-xl text-center py-32 lg:py-64">
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
-          Start by testing your limits.
+          Start by <span className="text-purple-400 underline">testing</span> your limits.
         </h1>
         <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
-          Create interactive quizzes for yourself, your students, and others here with Canute.
+          Create interactive quizzes for yourself, your students, and others
+          here with Canute.
         </p>
         <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-          <a
-            href="#"
-            className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 dark:focus:ring-purple-900"
-          >
-            Get started
-            <svg
-              className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </a>
-          <a
-            href="#"
-            className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
-          >
-            Learn more
-          </a>
+          <HeroForm/>
         </div>
-        {/* <div className="mt-8">
-          <form className="max-w-sm mx-auto">
-            <input
-              type="number"
-              id="number-input"
-              aria-describedby="helper-text-explanation"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="90210"
-              required
-            />
-          </form>
-        </div> */}
       </div>
     </section>
   );
--- components/partials/Footer.tsx
+++ components/partials/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className=" fixed bottom-0 left-0 z-20 w-full p-40 md:flex md:items-center md:justify-between md:p-6">
       <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        © 2023 Canute™. All Rights Reserved.
+        © 2023 Canute by <span className="text-purple-500 hover:underline hover:text-purple-400"><a href="http://www.github.com/abyanmajid">Abyan Majid</a></span>
       </span>
     </footer>
   );
