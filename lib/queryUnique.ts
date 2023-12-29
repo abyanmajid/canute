3d8c57f56ab8ac8482342c17daf55ed4babeb7a6
kinderheim511 <abyan@abydyl.net>
2023-12-29 23:53:58 +0700

    Update queryUnique.ts

--- lib/queryUnique.ts
+++ lib/queryUnique.ts
@@ -4,7 +4,7 @@ import User from "@/models/user";
 import connectMongoDB from "./mongodb";
 
 export async function isUsernameTaken(str: string) {
-  connectMongoDB();
+  await connectMongoDB();
   const exists = await User.exists({
     username: { $regex: str, $options: "i" },
   });
@@ -12,7 +12,7 @@ export async function isUsernameTaken(str: string) {
 }
 
 export async function isEmailTaken(email: string, typeAccount: string) {
-  connectMongoDB();
+  await connectMongoDB();
   const exists = await User.exists({
     email: { $regex: email, $options: "i" },
     typeAccount: {$regex: typeAccount, $options: "i"}
