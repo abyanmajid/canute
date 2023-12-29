19c14431cf28394c7a03fd0dec3963532508f900
kinderheim511 <abyan@abydyl.net>
2023-12-29 22:41:26 +0700

    Update options.ts

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -68,6 +68,7 @@ export const options = {
               typeAccount: typeAccount,
             }),
           });
+          new Promise((resolve) => setTimeout(resolve, 5000));
           if (res.ok) {
             return user;
           }
