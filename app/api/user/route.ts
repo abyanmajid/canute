f043d7794c5c5de90f48d7b7d2dcfa903ffd4780
kinderheim511 <abyan@abydyl.net>
2023-12-30 12:57:44 +0700

    Update route.ts

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -3,8 +3,13 @@ import User from "@/models/user";
 import { NextResponse } from "next/server";
 
 export async function POST(request: any) {
+  console.log("GOT HERE 1");
   const { username, email, typeAccount } = await request.json();
+  console.log("GOT HERE 2");
   await connectMongoDB();
+  console.log("GOT HERE 3");
   await User.create({ username, email, typeAccount });
+  console.log("GOT HERE 4");
   return NextResponse.json({ message: "User Registered" }, { status: 201 });
+  console.log("GOT HERE 5");
 }
